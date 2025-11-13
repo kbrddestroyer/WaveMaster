e00449cae4938f032d6dd47dd779df1053be73c8
Keyboard Destroyer <grudko2003@gmail.com>
2025-11-13 20:58:11 +0300

    Changed level switcher

--- Source/WaveMaster/InGameLevelSwitcher.cpp
+++ Source/WaveMaster/InGameLevelSwitcher.cpp
@@ -2,20 +2,28 @@
 
 
 #include "InGameLevelSwitcher.h"
-#include "Kismet/GameplayStatics.h"
+
+AInGameLevelSwitcher * AInGameLevelSwitcher::Instance;
+
 
 // Sets default values
 AInGameLevelSwitcher::AInGameLevelSwitcher()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void AInGameLevelSwitcher::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (Instance)
+	{
+		return;
+	}
+
+	Instance = this;
 }
 
 // Called every frame
@@ -34,9 +42,45 @@ void AInGameLevelSwitcher::Tick(float DeltaTime)
 	}
 }
 
-void AInGameLevelSwitcher::TriggerLevelSwitch() const
+void AInGameLevelSwitcher::BeginDestroy()
 {
-	UGameplayStatics::OpenLevel(
-		this, NextLevelName
-	);
+	Instance = nullptr;
+	Super::BeginDestroy();
+}
+
+
+void AInGameLevelSwitcher::TriggerLevelSwitch()
+{
+	DestroyOldGeometry();
+	CreateNewGeometry();
+}
+
+void AInGameLevelSwitcher::DestroyOldGeometry()
+{
+	if (!CurrentGeometry)
+		return;
+	
+	CurrentGeometry->Destroy();
+	CurrentGeometry = nullptr;
+}
+
+void AInGameLevelSwitcher::CreateNewGeometry()
+{
+	if (!NextGeometry)
+		return;
+
+	FActorSpawnParameters Params;
+	Params.Owner = nullptr;
+	Params.Instigator = GetInstigator();
+	Params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
+	const FVector Position = GetActorLocation();
+	const FRotator Rotation = FRotator::ZeroRotator;
+	
+	CurrentGeometry = GetWorld()->SpawnActor<AActor>(
+			NextGeometry,
+			Position,
+			Rotation,
+			Params
+		);
 }
--- Source/WaveMaster/InGameLevelSwitcher.h
+++ Source/WaveMaster/InGameLevelSwitcher.h
@@ -10,10 +10,15 @@ UCLASS()
 class WAVEMASTER_API AInGameLevelSwitcher : public AActor
 {
 	GENERATED_BODY()
+
+private:
+	static AInGameLevelSwitcher * Instance;
 	
 public:	
 	// Sets default values for this actor's properties
 	AInGameLevelSwitcher();
+
+	inline static AInGameLevelSwitcher * GetInstance() { return Instance; }
 
 protected:
 	// Called when the game starts or when spawned
@@ -22,14 +27,24 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
+	virtual void BeginDestroy() override;
+	
 	/**
 	 * Function that triggers level switch
 	 * Param NextLevelName should be set to valid value in editor!
 	 */
 	UFUNCTION()
-	void TriggerLevelSwitch() const;
+	void TriggerLevelSwitch();
+private:
+
+	void DestroyOldGeometry();
+	void CreateNewGeometry();
+	
 protected:
+	// std::vector ? 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Level Switcher")
-	FName NextLevelName;
+	TSubclassOf<AActor> NextGeometry;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Level Switcher")
+	AActor* CurrentGeometry;
 };
